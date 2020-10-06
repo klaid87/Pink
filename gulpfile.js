21ef77c89a857ccf8afc5dc1b40055396b112c0c
Artem <klaid-87@mail.ru>
2020-10-06 19:15:41 +0300

    Правки кода

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ sass.compiler = require('node-sass');
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
 
 gulp.task("style", function(done) {
@@ -16,9 +20,28 @@ gulp.task("style", function(done) {
       autoprefixer()
     ]))
     .pipe(gulp.dest("src/css"))
+    .pipe(minify())
+    .pipe(rename("style-min.css"))
+    .pipe(gulp.dest("src/css"))
     .pipe(server.stream());
 
   done();
+});
+
+gulp.task("images", function() {
+  return gulp.src("src/images/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progresiv: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("src/images"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("src/images/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("src/images"));
 });
 
 gulp.task("serve", function(done) {
